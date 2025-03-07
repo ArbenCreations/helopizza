524ac2f427c470535cfde1e26fd144f56bc5529f
Harman <hamu.dhillon@gmail.com>
2025-03-08 01:47:40 +0530

    fix

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('menu/',menu,name='menu'),
+    path('reservation/',reservation,name='reservation'),
+    path('contact/',contact,name='contact'),
 ]
--- base/views.py
+++ base/views.py
@@ -6,3 +6,18 @@ from django.shortcuts import render
 def home(request):
 
     return render(request,'home.html')
+
+
+
+def menu(request):
+
+    return render(request,'menu.html')
+
+
+def reservation(request):
+
+    return render(request,'reservation.html')
+
+def contact(request):
+
+    return render(request,'contact.html')
