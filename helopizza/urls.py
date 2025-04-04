fffe0864fc4c1f2842e04fd4cbf4188e99d98b13
Harman <hamu.dhillon@gmail.com>
2025-04-04 22:17:09 +0530

    app

--- helopizza/urls.py
+++ helopizza/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('base.urls'))
+    path('app/',include('base.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
